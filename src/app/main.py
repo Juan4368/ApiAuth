337019e442236af96c0de9b193f6656131cf0d07
Juanfer velez <juanfercho4368@gmail.com>
2026-03-07 22:39:57 -0500

    se modifica CORS

--- src/app/main.py
+++ src/app/main.py
@@ -52,7 +52,7 @@ def create_app() -> FastAPI:
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://192.168.1.9:5173",
-        "https://api.seustech.com",
+        "https://pos.seustech.com",
     ]
 
     app.add_middleware(
